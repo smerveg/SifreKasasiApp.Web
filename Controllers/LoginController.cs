80f0de640400fab3b50e3e287ace74bcee6b1421
smerv <smerv@DESKTOP-6AIGJPQ>
2023-09-08 16:08:29 +0300

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SifreKasasiApp.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
